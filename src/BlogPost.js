a5f3ab2e3bd868576e2835c0d43c1aa9d517540e
wilfredmulenga <wilfred@zazuafrica.com>
2019-04-18 11:36:47 +0200

    able to get the list of blogs and when blog is clicked routes to show details of the blog

---
+++ src/BlogPost.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import butter from './butter-client'
+
+class BlogPost extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: false
+    };
+  }
+
+  componentWillMount() {
+    let slug = this.props.params.slug;
+
+    butter.post.retrieve(slug).then((resp) => {
+      this.setState({
+        loaded: true,
+        post: resp.data.data
+      })
+    });
+  }
+
+  render() {
+    if (this.state.loaded) {
+      const post = this.state.post;
+
+      return (
+        <div>
+          <h1>{post.title}</h1>
+          <div dangerouslySetInnerHTML={{__html: post.body}} />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          Loading...
+        </div>
+      );
+    }
+  }
+}
+
+export default BlogPost;
---
+++ src/butter-client.js
@@ -0,0 +1,5 @@
+import Butter from 'buttercms'
+
+const butter = Butter('8f058badc30a24d1e1d3699e1945cff6b29a4892')
+
+export default butter
