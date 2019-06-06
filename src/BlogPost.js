c17da951400bcf166f071c1a75d89fcaba58172a
wilfredmulenga <wilfred@zazuafrica.com>
2019-06-06 22:19:18 +0200

    add meta tags to files

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -29,6 +29,8 @@ class BlogPost extends Component {
       return (
         <div>
           <h1>{post.title}</h1>
+          <meta property="og:description" content="How much does culture influence creative thinking?" />
+          <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
           <div dangerouslySetInnerHTML={{__html: post.body}} />
         </div>
       );
