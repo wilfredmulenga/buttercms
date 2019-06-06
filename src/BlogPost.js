d27b61b3c0ecfec94ff85a022e9459d313be0ee5
wilfredmulenga <wilfred@zazuafrica.com>
2019-06-06 23:26:05 +0200

    1

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -32,7 +32,7 @@ class BlogPost extends Component {
           <h1>{post.title}</h1>
           <Helmet>
           <meta property="og:description" content="How much does culture influence creative thinking?" />
-          <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+          {/* <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" /> */}
           </Helmet>
           <div dangerouslySetInnerHTML={{__html: post.body}} />
         </div>
