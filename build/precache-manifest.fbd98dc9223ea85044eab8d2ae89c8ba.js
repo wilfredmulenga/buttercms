690abf099be4f3903cc1ac1a42b2b9f2157d57df
wilfredmulenga <wilfred@zazuafrica.com>
2019-06-07 10:08:09 +0200

    add build folder to git

---
+++ build/precache-manifest.fbd98dc9223ea85044eab8d2ae89c8ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e4215708308882fa4593",
+    "url": "/static/js/main.8eabbebf.chunk.js"
+  },
+  {
+    "revision": "5583af494387526b1adb",
+    "url": "/static/js/2.f0ce683e.chunk.js"
+  },
+  {
+    "revision": "f2447ddb5cbe9d0732f986240430c8d3",
+    "url": "/index.html"
+  }
+];
