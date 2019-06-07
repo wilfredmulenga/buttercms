83b7867df7b355cb65ac43274caa538a69f42d28
wilfredmulenga <wilfred@zazuafrica.com>
2019-06-07 09:52:18 +0200

    add react snapshot

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import { browserHistory } from 'react-router';
 
 import Routes from './routes';
 
-ReactDOM.render(
- <Routes history={browserHistory} />,
+ render(<Routes history={browserHistory} />,
  document.getElementById('root')
-);
+)
