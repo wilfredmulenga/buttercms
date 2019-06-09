83c6d20ce8191e80b027fb8c30f6ccf87c56dabc
wilfredmulenga <wilfred@zazuafrica.com>
2019-06-09 08:15:15 +0200

    rebuild build file

---
+++ build/precache-manifest.246d0d37960338dbdf29643a23166fdd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fdcf6202bfc4ccc1f75c",
+    "url": "/static/js/main.2515cb63.chunk.js"
+  },
+  {
+    "revision": "5583af494387526b1adb",
+    "url": "/static/js/2.f0ce683e.chunk.js"
+  },
+  {
+    "revision": "c8332ef2720ca0343ced40439a80fad3",
+    "url": "/index.html"
+  }
+];
